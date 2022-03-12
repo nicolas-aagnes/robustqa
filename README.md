fb82cf1a05a52e143bf243923cb865bc68b4618c
Nicolas Aagnes <nicolas.aagnes@gmail.com>
2022-03-12 11:33:44 -0800

    notebook for testing

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 - Train a baseline MTL system with `python train.py --do-train --eval-every 2000 --run-name baseline`
 - Evaluate the system on test set with `python train.py --do-eval --sub-file mtl_submission.csv --save-dir save/baseline-01`
 - Upload the csv file in `save/baseline-01` to the test leaderboard. For the validation leaderboard, run `python train.py --do-eval --sub-file mtl_submission_val.csv --save-dir save/baseline-01 --eval-dir datasets/oodomain_val`
+- NB: Use svl tmux session, that uses a titanrtx gpu on svl-interactive cluster.
--- args.py
+++ args.py
@@ -1,13 +1,14 @@
 import argparse
 
 
-def get_train_test_args():
+def get_train_test_args(args=None):
     parser = argparse.ArgumentParser()
     parser.add_argument("--batch-size", type=int, default=16)
     parser.add_argument("--num-epochs", type=int, default=10)
     parser.add_argument("--lr", type=float, default=3e-5)
     parser.add_argument("--num-visuals", type=int, default=10)
     parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--finetune-path", type=str, default=None)
     parser.add_argument("--save-dir", type=str, default="save/")
     parser.add_argument("--train", action="store_true")
     parser.add_argument("--eval", action="store_true")
@@ -27,5 +28,5 @@ def get_train_test_args():
     parser.add_argument("--sub-file", type=str, default="")
     parser.add_argument("--visualize-predictions", action="store_true")
     parser.add_argument("--eval-every", type=int, default=5000)
-    args = parser.parse_args()
+    args = parser.parse_args() if args is None else parser.parse_args(args=args)
     return args
